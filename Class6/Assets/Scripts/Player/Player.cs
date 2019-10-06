b757270d90b2e0fea68b7588a2bb64ef2bf3c8d2
Dan Marquis <daniel.marquis@uconn.edu>
2019-10-06 13:42:42 -0400

    Initial project commit

---
+++ Class6/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public int health = 3;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void collidedWithEnemy(Enemy enemy)
+	{
+		enemy.Attack(this);
+		// Enemy attack code
+		if (health <= 0)
+		{
+		}
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		Enemy enemy = col.collider.gameObject.GetComponent<Enemy>();
+        if(enemy)
+		{
+			collidedWithEnemy(enemy);
+		}
+	}
+
+}
